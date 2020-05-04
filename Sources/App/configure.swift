d254267c587060b545c736c276963db46e76143f
Jacob Jheng <jacob@staging.com.tw>
2020-05-04 14:16:32 +0800

    Fixed code for postgresql database.

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -23,11 +23,35 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     // Register the configured SQLite database to the database config.
     var databases = DatabasesConfig()
     
-    let databaseConfig = PostgreSQLDatabaseConfig(
-        hostname: "localhost",
+    let databaseConfig: PostgreSQLDatabaseConfig
+    if let url = Environment.get("postgres://bsiuvbgsfilmwh:ed7967d6c4552b706ec65f00f35921f724188a7dc9340916c95478e4d219269f@ec2-50-17-90-177.compute-1.amazonaws.com:5432/de0tm180s9eb1u") {
+        databaseConfig = PostgreSQLDatabaseConfig(url: url)!
+    }
+    else {
+        let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
+        let databaseName: String
+        let databasePort: Int
+        if env == .testing {
+            databaseName = "vapor-test"
+            if let testPort = Environment.get("DATABASE_PORT") {
+                databasePort = Int(testPort) ?? 5433
+            }
+            else {
+                databasePort = 5433
+            }
+        }
+        else {
+            databaseName = "vapor"
+            databasePort = 5432
+        }
+        
+        databaseConfig = PostgreSQLDatabaseConfig(
+        hostname: hostname,
+        port: databasePort,
         username: "vapor",
-        database: "vapor",
+        database: databaseName,
         password: "password")
+    }
     let database = PostgreSQLDatabase(config: databaseConfig)
 
 
